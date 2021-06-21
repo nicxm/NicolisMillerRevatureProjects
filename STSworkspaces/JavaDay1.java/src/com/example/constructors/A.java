5e5e6f97d8194a196b5e0b9536a38f0bdba52fd9
Nicolis Miller <85629474+nicxm@users.noreply.github.com>
2021-06-21 16:38:51 -0500

    Update

---
+++ STSworkspaces/JavaDay1.java/src/com/example/constructors/A.java
@@ -0,0 +1,16 @@
+package com.example.constructors;
+
+public class A {
+private int myNumber;
+
+	public A() {
+		//call to super is implicit
+		System.out.println("");
+	}
+	
+	public A(int num) {
+		super();
+		System.out.println("");
+	}
+
+}
---
+++ STSworkspaces/JavaDay1.java/src/com/example/constructors/B.java
@@ -0,0 +1,15 @@
+package com.example.constructors;
+
+public class B {
+
+	private int myNumber;
+	public B() {
+		System.out.println("");
+	}
+	
+	public B(int num) {
+		//super(num);
+		System.out.println("");
+		this.myNumber = num;
+	}
+}
