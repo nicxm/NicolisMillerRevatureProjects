fe64d016e63289377d38f35ee26862813ad3db76
Nicolis Miller <85629474+nicxm@users.noreply.github.com>
2021-06-22 15:25:28 -0500

    update

---
+++ STSworkspaces/calc/src/calc/Calculator.java
@@ -0,0 +1,18 @@
+package calc;
+
+public class Calculator {
+
+	public int add(int x, int y) {
+		return x + y;
+	}
+
+	public int sub(int x, int y) {
+		return x - y;
+	}
+	public int divide(int x, int y) throws Exception{
+		return x / y;
+	}
+	public int multiply(int x, int y) {
+		return x *+ y;
+	}
+}
